513bd1450a664cb0b0c0eb3d4d1e9bb5d5312fb3
Arthur <arthur.vasylchyk@vaimo.com>
2023-03-29 20:41:26 +0300

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# vue3-test-task
+
+## Project setup
+```
+npm install
+```
+
+### Start Vite dev server in the current directory
+```
+npm run dev
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Locally preview production build
+```
+npm run serve
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <helloworld />
+    </v-main>
+  </v-app>
+</template>
+<script>
+import { defineComponent } from 'vue'
+import helloworld from './components/helloworld.vue'
+
+export default defineComponent({
+  name: 'App',
+
+  components: {
+    helloworld,
+  },
+
+  setup() {
+    // Data goes here
+    return {}
+  }
+})
+</script>
